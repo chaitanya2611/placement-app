22dbc1f45da1ba544ff6ca4aed945ba0b5bc7aa8
chaitanya2611 <115576523+chaitanya2611@users.noreply.github.com>
2026-08-07 03:07:44 +0530

    Add password reset fields

--- backend/models/User.js
+++ backend/models/User.js
@@ -17,6 +17,16 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    resetPasswordToken: {
+      type: String,
+      default: null,
+      select: false,
+    },
+    resetPasswordExpires: {
+      type: Date,
+      default: null,
+      select: false,
+    },
   },
   { timestamps: true },
 );
